9a5bb51f396bc5290bbf3c53b8b13cc883d71185
Shubha Guha <shubha.g@gmail.com>
2022-05-19 16:19:35 +0200

    Nitpick: Put main functionality in functions

--- main.py
+++ main.py
@@ -6,28 +6,38 @@ import config
 from experiment import experiment
 from relation import populate
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--run_experiments', default=False, action='store_true')
-parser.add_argument('--run_analysis', default=False, action='store_true')
-parser.add_argument('--cpu', default=1, type=int)
-parser.add_argument('--error_type', default=None)
-parser.add_argument('--seeds', default=None, type=int, nargs='+')
-parser.add_argument('--log', default=False, action='store_true')
-parser.add_argument('--dataset', default=None)
-parser.add_argument('--nosave', default=False, action='store_true')
-parser.add_argument('--alpha', default=0.05, type=float)
 
-args = parser.parse_args()
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_experiments', default=False, action='store_true')
+    parser.add_argument('--run_analysis', default=False, action='store_true')
+    parser.add_argument('--cpu', default=1, type=int)
+    parser.add_argument('--error_type', default=None)
+    parser.add_argument('--seeds', default=None, type=int, nargs='+')
+    parser.add_argument('--log', default=False, action='store_true')
+    parser.add_argument('--dataset', default=None)
+    parser.add_argument('--nosave', default=False, action='store_true')
+    parser.add_argument('--alpha', default=0.05, type=float)
 
-# run experiments on datasets
-if args.run_experiments:
-    print("=== Running experiments ===")
-    datasets = [utils.get_dataset(args.dataset)] if args.dataset is not None else config.datasets
-    experiment(datasets, args.log, args.cpu, args.nosave, args.error_type, args.seeds)
+    return parser.parse_args()
 
-# run analysis on results
-if args.run_analysis:
-    print("=== Running analysis ===")
-    populate([args.alpha])
 
-print("Done")
+def main():
+    args = _parse_args()
+
+    # run experiments on datasets
+    if args.run_experiments:
+        print("=== Running experiments ===")
+        datasets = [utils.get_dataset(args.dataset)] if args.dataset is not None else config.datasets
+        experiment(datasets, args.log, args.cpu, args.nosave, args.error_type, args.seeds)
+
+    # run analysis on results
+    if args.run_analysis:
+        print("=== Running analysis ===")
+        populate([args.alpha])
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
