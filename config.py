af0600daeeb51f88ac26aeccacb3e8840f37e350
Shubha Guha <shubha.g@gmail.com>
2022-05-19 16:20:32 +0200

    Nitpick: Prettier comment formatting, easier to read

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ from schema import dataset, model, error_type, scenario
 # =============================================================================
 # Directory Configuration
 # =============================================================================
-data_dir = 'data' # dir storing data
-result_dir = 'result' # dir saving experiment results
-analysis_dir = 'analysis' # dir saving analysis results
-plot_dir = 'plot' # dir saving plots
+data_dir = 'data'          # dir storing data
+result_dir = 'result'      # dir saving experiment results
+analysis_dir = 'analysis'  # dir saving analysis results
+plot_dir = 'plot'          # dir saving plots
 
 # =============================================================================
 # Experiment Configuration
 # =============================================================================
-root_seed = 1 # root seed for entire experiments
-n_resplit = 20 # num of resplit for handling split randomness
-n_retrain = 5 # num of retrain for handling random search randomness
-test_ratio = 0.3 # train/test ratio
-max_size = 15000 # max data size for training
+root_seed = 1     # root seed for entire experiments
+n_resplit = 20    # num of resplit for handling split randomness
+n_retrain = 5     # num of retrain for handling random search randomness
+test_ratio = 0.3  # train/test ratio
+max_size = 15000  # max data size for training
 
 # =============================================================================
 # Schema Configuration
