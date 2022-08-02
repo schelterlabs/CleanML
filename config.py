aee395ea05eb43a8d68fb8cd1ce1fecd87d56f7c
Shubha Guha <shubha.g@gmail.com>
2022-08-02 12:37:13 +0200

    Increased n_resplits to 20 again

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ plot_dir = 'plot'          # dir saving plots
 # Experiment Configuration
 # =============================================================================
 root_seed = 1     # root seed for entire experiments
-n_resplit = 5     # num of resplit for handling split randomness
+n_resplit = 20    # num of resplit for handling split randomness
 n_retrain = 5     # num of retrain for handling random search randomness
 test_ratio = 0.3  # train/test ratio
 max_size = 15000  # max data size for training
