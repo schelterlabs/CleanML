f15cd1fcedb8fe1ea6161a3e3d9a628f9f9f5ea8
Shubha Guha <shubha.g@gmail.com>
2022-08-26 14:13:22 +0200

    Nitpick: Removed whitespace

--- experiment.py
+++ experiment.py
@@ -30,7 +30,7 @@ def one_search_experiment(dataset, error_type, train_file, model, seed, n_jobs=1
     X_train, y_train, X_test_list, y_test_list, test_group_memberships, test_files = \
         preprocess(dataset, error_type, train_file, normalize=True, down_sample_seed=down_sample_seed)
 
-    test_files = list(set(test_files).difference(set(skip_test_files)))   
+    test_files = list(set(test_files).difference(set(skip_test_files)))
 
     # train and evaluate
     result = train_and_evaluate(X_train, y_train, X_test_list, y_test_list, test_group_memberships,
